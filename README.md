25355dcaed675b0941b0bd08860b310f49a9c73e
“你littleliu <522448949@qq.com>
2021-04-01 17:13:15 +0800

    update

--- README.md
+++ README.md
@@ -7,8 +7,11 @@
 [Github deploy key](https://docs.github.com/en/developers/overview/managing-deploy-keys#deploy-keys)  
 [Bootstrap](https://v3.bootcss.com/getting-started/)  
 [Github OAuth](https://docs.github.com/en/developers/apps/creating-an-oauth-app)  
-[Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
+[Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)  
+[菜鸟教程](https://www.runoob.com/mysql/mysql-insert-query.html)  
+[Thymleaf](https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html#setting-attribute-values)
 
 ## 工具
 [Git](http://git-scm.com/download)  
-[Visual Paradigm](http://www.visual-paradigm.com)
+[Visual Paradigm](http://www.visual-paradigm.com)  
+[Lombok](https://projectlombok.org/)
--- src/main/java/life/littleliu/community/controller/IndexController.java
+++ src/main/java/life/littleliu/community/controller/IndexController.java
@@ -1,7 +1,11 @@
 package life.littleliu.community.controller;
 
+import life.littleliu.community.dto.QuestionDTO;
+import life.littleliu.community.mapper.QuestionMapper;
 import life.littleliu.community.mapper.UserMapper;
+import life.littleliu.community.model.Question;
 import life.littleliu.community.model.User;
+import life.littleliu.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -17,19 +22,27 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie:cookies){
-            if(cookie.getName().equals("token")){
-                String token=cookie.getValue();
-                User user=userMapper.findByToken(token);
-                if(user!=null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies != null && cookies.length != 0)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
-        }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
